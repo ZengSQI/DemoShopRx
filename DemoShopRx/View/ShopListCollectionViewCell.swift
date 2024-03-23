27c2063e5487bd0242458f821d9d081fe34afa42
Steven Zeng <zengsqi199@gmail.com>
2024-03-23 15:58:46 +0800

    setup shop list view

---
+++ DemoShopRx/View/ShopListCollectionViewCell.swift
@@ -0,0 +1,71 @@
+//
+//  ShopListCollectionViewCell.swift
+//  DemoShopRx
+//
+//  Created by Steven Zeng on 2024/3/23.
+//
+
+import UIKit
+import SnapKit
+
+class ShopListCollectionViewCell: UICollectionViewCell {
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = .preferredFont(forTextStyle: .caption1)
+        return label
+    }()
+
+    private lazy var priceLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = .preferredFont(forTextStyle: .caption1)
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSubview()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupSubview() {
+        contentView.addSubview(imageView)
+        contentView.addSubview(nameLabel)
+        contentView.addSubview(priceLabel)
+
+        imageView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(contentView)
+        }
+
+        nameLabel.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(8)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.height.equalTo(16)
+        }
+
+        priceLabel.snp.makeConstraints { make in
+            make.top.equalTo(nameLabel.snp.bottom).offset(8)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.bottom.equalToSuperview().offset(-16)
+            make.height.equalTo(16)
+        }
+    }
+
+    func bind(item: ShopItem) {
+        imageView.image = UIImage(named: item.imageName)
+        nameLabel.text = item.name
+        priceLabel.text = "$ " + item.price.formatted()
+    }
+}
--- DemoShopRx/ViewController/ShopListViewController.swift
+++ DemoShopRx/ViewController/ShopListViewController.swift
@@ -10,6 +10,32 @@ import RxSwift
 import RxCocoa
 
 class ShopListViewController: UIViewController {
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(ShopListCollectionViewCell.self, forCellWithReuseIdentifier: "ShopListCollectionViewCell")
+        return collectionView
+    }()
+
+    private lazy var layout: UICollectionViewLayout = {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(0.5),
+            heightDimension: .fractionalWidth(0.5)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalWidth(0.5)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            subitems: [item]
+        )
+        group.interItemSpacing = .flexible(16)
+        group.contentInsets = .init(top: 16, leading: 16, bottom: 0, trailing: 16)
+        let section = NSCollectionLayoutSection(group: group)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }()
 
     private var viewModel: ShopListViewModel!
     private var disposeBag = DisposeBag()
@@ -21,18 +47,26 @@ class ShopListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupSubview()
         bindViewModel()
+        title = "商品列表"
     }
 
-    func bindViewModel() {
+    private func setupSubview() {
+        view.addSubview(collectionView)
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+
+    private func bindViewModel() {
         let output = viewModel.transform(
             input: ShopListViewModel.Input(load: .just(()))
         )
 
         output.list
-            .debug()
-            .bind { items in
-                print(items)
+            .bind(to: collectionView.rx.items(cellIdentifier: "ShopListCollectionViewCell", cellType: ShopListCollectionViewCell.self)) { index, model, cell in
+                cell.bind(item: model)
             }
             .disposed(by: disposeBag)
     }
